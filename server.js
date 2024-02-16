a90619e6e077811c87ccf3f98593862de2169ed4
john mbugua <johnymbugua1997@gmail.com>
2024-02-16 14:45:01 +0300

    addtional express and node js course

--- server.js
+++ server.js
@@ -9,13 +9,30 @@
 // port number are like doors inot a computers/ software or server communicate to
 
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
     console.log("Home page working");
     res.setHeader("content-type", "text/html");
-    res.write("<h1>Creating web page using the node basics</h1>");
-    res.write("<h1>Backing a backend developer</h1>");
+    // res.write("<h1>Creating web page using the node basics</h1>");
+    // res.write("<h1>Backing a backend developer</h1>");
+    // res.end();
+
+    // read the file
+
+    fs.readFile("./public/index.html", (err, data) => {
+      if (err) {
+        console.log(err);
+        res.end();
+      } else {
+        res.write(data);
+        res.end();
+
+        // NB:: You can mostly res.write to send multiple docs
+        // but also another way of sending data could res.end(data)
+      }
+    });
   }
 });
 

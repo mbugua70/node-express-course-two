ff3706a05d07b5516ffb4a3e5d3af944c7b0d67b
john mbugua <johnymbugua1997@gmail.com>
2024-02-26 19:12:57 +0300

    additional changes

--- ejs_two.js
+++ ejs_two.js
@@ -1,14 +1,3 @@
-// Middleware
-// code which runs (on the server) btn getting the request and sending response
-// middleware run from top to bottom
-// they stop to exercute once response is being send or been send to the next stage
-
-// uses of middleware
-// logger middleware log details of every request
-// Authentication check middleware for protected routes
-// middleware to parse JSON data from request
-// return 404 pages
-
 // view engines / Template engines
 
 // allow us to write dynimic websites but also inject data to our websites
@@ -20,29 +9,13 @@
 const express = require("express");
 const app = express();
 
-// connect to database(mongodb)
-const dbURL =
-  "mongodb+srv://myAtlasDBUser:123455@myatlasclusteredu.kmyoknr.mongodb.net/?retryWrites=true&w=majority";
-
 // The below code is used to configure the view engine with ejs
 app.set("view engine", "ejs");
-
-// static files using middleware
-
-app.use(express.static("public"));
 
 // Below code shows how we can configure ejs using different directory.
 // app.set("views", "public");
 
 // sending html files
-
-// the use of middleware
-app.use((req, res, next) => {
-  console.log(req.hostname);
-  console.log(req.path);
-  console.log(req.method);
-  next();
-});
 
 app.get("/", (req, res) => {
   //   res.sendFile("./public/index.html", { root: __dirname });
@@ -95,8 +68,8 @@ app.listen(port, () => {
 });
 
 // NOTES:
-// 3rd Party Middleware
-// example is morgan libraly
+// Our EJS templates are processed through the ESJ view engine on the server.
 
-// static files using middleware
-// e.g css files
+// PARTIALS
+// THE use of <%- %> and include method
+// <%= %> when we use = sign we except special character. ending up with string values instead of raw html
